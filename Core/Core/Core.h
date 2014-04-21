d99a4bf8cc05152ca13da7d63afb463d14176393
hanjize <hanjize@rd.tuan800.com>
2014-04-21 18:17:11 +0800

    添加核心功能文件
    添加核心功能文件

---
+++ Core/Core/Core.h
@@ -0,0 +1,34 @@
+
+// The general purpose logger. This ignores logging levels.
+
+
+//Reachability 
+
+//fmdb
+#import "Core/FMDatabase.h"
+#import "Core/FMDatabaseAdditions.h"
+
+#import "Core/NSDictionaryAdditions.h"
+
+//json
+#import "Core/JSON.h"
+
+#import "Core/TBFileUtil.h"
+#import "Core/NSString+Addition.h"
+ 
+#import "Core/TBCoreMacros.h"
+
+
+#import "Core/TBSourceI18n.h"
+#import "Core/ImageCache.h" 
+
+#import "Core/RegexKitLite.h"
+#import "Core/QSStrings.h"
+
+#import "Core/TBCoreCommonFunction.h"
+
+#import "Core/TBCoreDao.h"
+#import "Core/TBCoreConfigDao.h"
+#import "Core/TBURLCache.h"
+
+#import "Core/NSObjectAdditions.h"
